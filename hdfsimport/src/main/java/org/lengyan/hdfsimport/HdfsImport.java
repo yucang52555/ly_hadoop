0114dfb9ee3b36cd19c241bd7d39397cd5223164
冷眼 <kangtiancheng001@163.com>
2017-12-23 11:00:30 +0800

    读取数据库数据写入HDFS

--- hdfsimport/src/main/java/org/lengyan/hdfsimport/HdfsImport.java
+++ hdfsimport/src/main/java/org/lengyan/hdfsimport/HdfsImport.java
@@ -1,7 +1,9 @@
 package org.lengyan.hdfsimport;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -9,10 +11,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
 import org.apache.hadoop.mapreduce.lib.db.DBInputFormat;
+import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.lengyan.bean.SinaWeibo;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Iterator;
 
 /**
@@ -39,20 +44,21 @@ public class HdfsImport {
     }
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("hadoop.home.dir", "E:\\ideaspace\\hadoop-2.7.4");
+        System.setProperty("hadoop.home.dir", "F:\\kingkang\\ly_hadoop\\hadoop-2.7.4");
         Configuration conf = new Configuration();
+        DBConfiguration.configureDB(conf,"com.mysql.jdbc.Driver", "jdbc:mysql://119.23.10.175:3306/ly_smart?characterEncoding=UTF-8","root","hongfeng123456");
         Job job = Job.getInstance(conf, "HdfsImport");
         job.setJarByClass(HdfsImport.class);
-        job.setMapperClass(HdfsImportMapper.class);        job.setReducerClass(HdfsImportReducer.class);
+        job.setMapperClass(HdfsImportMapper.class);
+        job.setReducerClass(HdfsImportReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
-
-        DBConfiguration.configureDB(conf,"com.mysql.jdbc.Driver", "jdbc:mysql://119.23.10.175:3306/ly_smart?characterEncoding=UTF-8","root","hongfeng123456");
-        String [] fields = {"user_name", "user_url", "address", "abstractContent"};
+        String [] fields = {"weibo_url", "pub_time", "pub_content"};
         DBInputFormat.setInput(job, SinaWeibo.class,"T_SINA_WEIBO", null, null, fields);
-        FileOutputFormat.setOutputPath(job, new Path("hdfs://127.0.0.1:9000/hdfsFile"));
-
+        FileOutputFormat.setOutputPath(job, new Path("F:\\kingkang\\workspace\\ly_hadoop\\hdfsimport\\output"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
     }
 
 
